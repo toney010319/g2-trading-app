6b6dd645f0b566c3e30f0ecbb70bfd06ccfa93e1
andradajus <138541478+andradajus@users.noreply.github.com>
2023-12-18 17:14:03 +0800

    Initiate vite-react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <span className="text-2xl text-yellow-200">This is our homepage</span>
+      <span className="bg-red-200">Tailwind Test</span>
+      <span className="text-black">HMR test 2</span>
+
+    </>
+  )
+}
+
+export default App
