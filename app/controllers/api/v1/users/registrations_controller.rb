ee2be2122dc95464ec8cf4a7501e6cb2b61a21bf
Toney010319 <toney010319@gmail.com>
2023-12-20 19:56:08 +0800

    transfer the registraticontroller to users folder

---
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -0,0 +1,33 @@
+ 
+module Api
+  module V1
+    module Users
+      class RegistrationsController < Devise::RegistrationsController
+        def create
+          puts params.inspect
+          user = User.new(sign_up_params)
+      
+          if user.save
+            render json: user
+          else
+            render json: { errors: user.errors }
+          end
+        end
+      
+        def index
+          render json: User.all
+        end
+      
+        def show
+          render json: User.find(params[:id])
+        end
+      
+        private
+      
+        def sign_up_params
+          params.require(:registration).permit(:username, :first_name, :middle_name, :last_name, :email, :birthday, :password)
+        end
+      end
+    end
+  end
+end
