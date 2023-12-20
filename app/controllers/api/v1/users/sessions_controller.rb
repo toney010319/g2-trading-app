70caae2e7e851c7cdb63b26ef11319054a4496ce
Toney010319 <toney010319@gmail.com>
2023-12-20 19:58:23 +0800

    creating sessions controller, and change the routes

---
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -0,0 +1,24 @@
+module Api
+  module V1
+    module Users
+      class SessionsController < ApplicationController
+        def create
+          user = User.find_by(email: params[:email])
+        
+          if user&.valid_password?(params[:password])
+            token = encode_token({ user_id: user.id })
+            render json: { user: user, token: token }, status: :ok
+          else
+            render json: { error: 'Invalid email or password' }, status: :unauthorized
+          end
+        end
+
+        private
+
+        def encode_token(payload)
+          JWT.encode(payload, '123456789')
+        end
+      end
+    end
+  end
+end
