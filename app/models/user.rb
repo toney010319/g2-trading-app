0034907e466c9b93a75f02961de2d6db512b2ecc
andrewizo <sanchezdenzelandrew@gmail.com>
2024-01-04 20:56:14 +0800

    update fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :api
-=======
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
   validate :at_least_18
@@ -24,5 +17,4 @@ class User < ApplicationRecord
          :rememberable,
          :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
->>>>>>> 79aa3000ef735b9e9d9822151f244e7b3be99554
 end
