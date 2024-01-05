ce26a6959354bbf65f2661c5447d9ca9a5be56b0
Toney010319 <toney010319@gmail.com>
2024-01-06 04:09:25 +0800

    Adding mailCatcher

---
+++ db/migrate/20240105192959_add_confirmable_to_devise.rb
@@ -0,0 +1,11 @@
+class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :confirmation_token, :string
+    add_index :users, :confirmation_token, unique: true
+
+    # User.reset_column_information # Need for some types of updates, but not for adding a column.
+    User.update_all(confirmed_at: Time.now) # For existing records without confirmable
+  end
+end
