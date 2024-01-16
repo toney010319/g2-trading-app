a38fdf24f1b00ce4f85c679fbf1fed190b4995ab
andradajus <138541478+andradajus@users.noreply.github.com>
2024-01-16 23:19:30 +0800

    Modified home components to initiate tokens before api calls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
     def show
         user = User.find(params[:user_id])
         render json: user
--- frontend/src/dashboard/DashboardLayout.jsx
+++ frontend/src/dashboard/DashboardLayout.jsx
@@ -1,7 +1,18 @@
 import Navigationbar from "../components/Navigationbar";
 import Sidebar from "../components/Sidebar";
+import { useEffect } from "react";
+import axios from "axios";
 // eslint-disable-next-line react/prop-types
 const DashboardLayout = ({ children, addAlert }) => {
+  useEffect(() => {
+    const initiateAuthorization = () => {
+        const token = document.cookie.split('token=')[1];
+        if (token) {
+            axios.defaults.headers.common['Authorization'] = token;
+        }
+    };
+    initiateAuthorization();
+}, []);
   return (
     <div className="flex h-screen">
       <Sidebar />
