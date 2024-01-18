5eb1256c0b19c17f386df2d2d94f65c5edf12c06
andradajus <138541478+andradajus@users.noreply.github.com>
2024-01-18 23:31:04 +0800

    Reverted sessions controller, created initial authorization setter on admin dashboard upon mount, explicitly added blank headers on log axios request

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Logo from "../assets/Logo";
 import useAuth from "../context/hooks/useAuth";
+import axios from "axios";
 // eslint-disable-next-line react/prop-types
 const Login = ({ addAlert }) => {
   const [agreed, setAgreed] = useState(false);
@@ -21,62 +22,51 @@ const Login = ({ addAlert }) => {
       },
     };
   
+    const axiosConfig = {
+      headers: {
+        Authorization: '',
+      },
+    };
+  
     try {
-      const response = await fetch("https://stellarmarkets-e9ba8be437a0.herokuapp.com/login", {
-        method: 'POST',
-        body: JSON.stringify(user),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-  
-      const data = await response.json();
-      const role = data?.data?.role;
+      const res = await axios.post("https://stellarmarkets-e9ba8be437a0.herokuapp.com/login", user, axiosConfig);
+      const role = res?.data?.data?.role;
   
       setAuth({ role: [role] });
-      return data;
+      return res;
     } catch (error) {
       return error;
     }
   };
 
-  // const res = await axios.post("http://localhost:3000/login", user);
-  
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    
-    try {
-      const res = await loginUser(event);
-  
-      if (res.status === 200) {
-        const token = res.headers.authorization;
-        const user_id = res.data.data.id;
-  
-        document.cookie = `token=${token};path=/`;
-        document.cookie = `user_id=${user_id};path=/`;
-  
-        addAlert("success", res.data.message);
-  
-        if (res.data.data.role === "Trader") {
-          navigate("/dashboard");
-        } else if (res.data.data.role === "Admin") {
-          navigate("/admin");
-        }
-      } else {
-        addAlert("error", res.response?.data);
-        navigate("/");
-      }
-    } catch (error) {
-      console.error("An error occurred:", error);
-    }
-  };
 
   return (
     <>
       <div className="flex flex-col justify-center items-center align-center content-center w-screen h-screen">
-        <form
-          onSubmit={handleSubmit}
+      <form
+          onSubmit={async (event) => {
+            event.preventDefault();
+            const res = await loginUser(event)
+            if (res.status == "200" && res.data.data.role === "Trader") {
+              const token = res.headers.authorization;
+              const user_id = res.data.data.id;
+              document.cookie = `token=${token};path=/`;
+              document.cookie = `user_id=${user_id};path=/`;
+              addAlert("success", res.data.message);
+              navigate("/dashboard");
+            } else if (res.status == "200" && res.data.data.role == "Admin") {
+              const token = res.headers.authorization;
+              const user_id = res.data.data.id;
+              document.cookie = `token=${token};path=/`;
+              document.cookie = `user_id=${user_id};path=/`;
+              addAlert("success", res.data.message);
+              navigate("/admin");
+            } else {
+              addAlert("error", res.response?.data);
+              navigate("/");
+
+            }
+          }}
           className="justify-center text-center align-center shadow-md border-md rounded-md  bg-gradient-to-b from-azure-300 to-azure-700 m-2 p-5 pl-8 pr-8"
         >
           <Logo />
