09f486e64984daac07faeda3744fa59bb0ddb3a7
Toney010319 <toney010319@gmail.com>
2024-01-19 13:39:02 +0800

    Changing API endpoin

--- frontend/src/components/subcomponents/Confirmation.jsx
+++ frontend/src/components/subcomponents/Confirmation.jsx
@@ -1,21 +1,18 @@
-// ConfirmationPage.js
+
 import { useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const ConfirmationPage = () => {
-    const { confirmation_token } = useParams(); // Adjust based on your routing params
+    const { confirmation_token } = useParams();
     const navigate = useNavigate();
 
     useEffect(() => {
-        // Send the confirmation token to the Rails API
         fetch(`http://localhost:3000/confirmation?confirmation_token=${confirmation_token}`)
             .then(response => response.json())
             .then(data => {
                 if (data.status === 'success') {
-                    // Redirect to the login page if confirmation was successful
                     navigate('http://127.0.0.1:5173/');
                 } else {
-                    // Handle any errors or show a message
                     console.error(data.message);
                 }
             })
